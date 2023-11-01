61b025bb1484a8f58bc077c56444b962a87e75e8
Swetha Sa <swethasakthi927@gmail.com>
2023-11-01 11:27:19 +0530

    commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let points = document.getElementById("points");
-let selectedP = document.getElementById("selectedP");
-let selectedC = document.getElementById("selectedC");
+let selected = document.getElementById("selected");
 let msg = document.getElementById("msg");
 let clear = document.getElementById("end");
 
 let randomNum = () => {
-  let materials = ["stone", "paper", "scissors"];
+  let materials = ["Stone", "Paper", "Scissors"];
   let randomValue = materials[Math.floor(Math.random() * 3)];
   return randomValue;
 };
@@ -26,11 +25,11 @@ let getResult = (playerChoice, computerChoice) => {
   let score;
   if (playerChoice === computerChoice) {
     score = 0;
-  } else if (playerChoice === "stone" && computerChoice === "scissors") {
+  } else if (playerChoice === "Stone" && computerChoice === "Scissors") {
     score = 1;
-  } else if (playerChoice === "paper" && computerChoice === "stone") {
+  } else if (playerChoice === "Paper" && computerChoice === "Stone") {
     score = 1;
-  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+  } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
     score = 1;
   } else {
     score = -1;
@@ -41,23 +40,83 @@ let getResult = (playerChoice, computerChoice) => {
 
 let showResult = (score, playerChoice, computerChoice) => {
   if (score === 0) {
-    points.innerText = 0;
     msg.innerText = `Match Draw😐`;
   } else if (score == 1) {
-    points.innerText = 1;
     msg.innerText = `You Won😍🤩`;
   } else {
-    points.innerText = -1;
     msg.innerText = `You Lose🥺`;
   }
-  selectedP.innerText = `Your's Choice ${playerChoice}`;
-  selectedC.innerText = `Computer's Choice ${computerChoice}`;
+  points.innerText = `Your Score 👉🏻 ${score} `;
+  selected.innerText = `${playerChoice} vs ${computerChoice}`;
 };
 
 let handleClear = () => {
   location.reload();
   points.innerText = "Score";
-  selectedP.innerText = `Your's Choice`;
-  selectedC.innerText = `Computer's Choice`;
+  selected.innerText = `Your's Choice`;
   msg.innerText = `Win or Lose`;
 };
+
+// let points = document.getElementById("points");
+// let selectedP = document.getElementById("selectedP");
+// let selectedC = document.getElementById("selectedC");
+// let msg = document.getElementById("msg");
+
+// let randomValue = () => {
+//   let options = ["stone", "paper", "scissors"];
+//   let randOptions = options[Math.floor(Math.random() * options.length)];
+//   return randOptions;
+// };
+
+// let playerValue = () => {
+//   let materials = document.querySelectorAll(".game");
+//   materials.forEach((element) => {
+//     element.onclick = () => {
+//       let playerOption = element.getAttribute("value");
+//       let computerChoice = randomValue();
+//       gameOn(playerOption, computerChoice);
+//     };
+//   });
+// };
+
+// playerValue();
+
+// let gameOn = (playerChoice, computerChoice) => {
+//   let score;
+
+//   if (playerChoice === computerChoice) {
+//     score = 0;
+//   } else if (
+//     (playerChoice === "stone" && computerChoice === "paper") ||
+//     (playerChoice === "paper" && computerChoice === "scissors") ||
+//     (playerChoice === "scissors" && computerChoice === "stone")
+//   ) {
+//     score = -1;
+//   } else {
+//     score = 1;
+//   }
+//   gameResult(score, playerChoice, computerChoice);
+//   return score;
+// };
+
+// let gameResult = (score, playerChoice, computerChoice) => {
+//   if (score === 0) {
+//     points.innerText = 0;
+//     msg.innerText = "Match Draw";
+//   } else if (score === -1) {
+//     points.innerText = -1;
+//     msg.innerText = "You Lose";
+//   } else {
+//     points.innerText = 1;
+//     msg.innerText = "You Won";
+//   }
+//   selectedP.innerText = `Your's Choice ${playerChoice}`;
+//   selectedC.innerText = `Computer's Choice ${computerChoice}`;
+// };
+
+// let handleClear = () => {
+//   points.innerText = "Score";
+//   msg.innerText = "Win or Lose";
+//   selectedP.innerText = `Your's Choice`;
+//   selectedC.innerText = `Computer's Choice`;
+// };
